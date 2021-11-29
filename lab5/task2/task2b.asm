9d0f0e725df38c6d263a44f66dff400f9789f61e
penne8 <penne8>
2021-11-29 03:07:02 +0200

    wip

--- lab5/task2/task2b.asm
+++ lab5/task2/task2b.asm
@@ -3,6 +3,11 @@ section .data
     word_count: dd 0
     read_count: dd 0
     in_word: db 0
+    ws_flag: db 0
+    word_offset: dd 0
+    good_word: db 0
+    ws_word: dd 0
+    file_des: dd 0
 
 section .text
 
@@ -22,46 +27,69 @@ _start:
 
 check_flag:
     cmp     BYTE [ecx], '-'
-    jne     open_file
+    jne     open_file           ; for non-w and non-ws occasions
 
     inc     ecx
     cmp     BYTE [ecx], 'w'
-    jne     open_file
+    jne     open_file           ; for non-w and non-ws occasions
+
+    inc     ecx
+
+    cmp     BYTE [ecx], 0
+    je      set_w_file_name     ; for w flag
+    
+    cmp     BYTE [ecx], 's'
+    jne     open_file           ; for non-w and non-ws occasions
 
     inc     ecx
     cmp     BYTE [ecx], 0
-    jne     open_file
-    
-    mov 	ecx, esp
-	add 	ecx, 12		; char *argv[1]
-    mov     ecx, [ecx]  ; char *str_file_name or flag
+    je      set_ws_file_name    ; for ws flag
 
+    jmp     open_file           ; for non-w and non-ws occasions
+
+set_w_file_name:
+    mov 	ecx, esp
+	add 	ecx, 12		    ; char *argv[1]
+    mov     ecx, [ecx]      ; char *str_file_name
+    jmp     cw_open_file    ; for w and ws occasions
+
+set_ws_file_name:
+    mov 	ecx, esp
+    add 	ecx, 12		            ; char *argv[1]
+    mov     ebx, [ecx]              ; using ebx for next mov op
+    mov     dword [ws_word], ebx    ; char *word
+
+	add 	ecx, 4		            ; char *argv[2]
+    mov     ecx, [ecx]              ; char *str_file_name
+
+    mov     BYTE [ws_flag], 1       ; light up ws flag
+    jmp     cw_open_file            ; for w and ws occasions
 
 cw_open_file:
     push    0           ; open file for read-only
     push    ecx         ; name of file
     call    open
-    mov     ebx, eax    ; fd
+    mov     [file_des], eax    ; file_des
 
 cw_read_file:
-    push    50          ; read 50 chars at a time
+    push    50                  ; read 50 chars at a time
     push    buff
-    push    ebx         ; fd
+    push    dword [file_des]    ; file_des
     call    read
     mov     [read_count], eax
 
 cw_check_EOF:
-    cmp     dword [read_count], 0      ; check if we read EOF
+    cmp     dword [read_count], 0   ; check if we read EOF
     je      write_count
 
-    mov     edx, 0
+    mov     edx, 0                  ; reset buffer offset
 
 count_words:
-    cmp     edx, dword [read_count]
-    je      cw_read_file
+    cmp     edx, dword [read_count]     ; if end of buffer reached
+    je      cw_read_file                ; take another buffer
 
     mov     eax, buff
-    add     eax, edx
+    add     eax, edx                    ; eax = curr char from buff
 
     cmp     BYTE [eax], 10  ; '\n'
     je      not_in_word
@@ -72,24 +100,66 @@ count_words:
     cmp     BYTE [eax], 9   ; '/t'
     je      not_in_word
 
-    jmp check_and_inc_in_word
+is_in_word:
+    cmp     BYTE [ws_flag], 1
+    je      ws_word_check
+
+w_word_check:
+    cmp     BYTE [in_word], 1   ; if not new word, skip to next char
+    je      next_char
+
+start_w_new_word:
+    inc     dword [word_count]  ; arrived to new word, inc count
+    mov     BYTE [in_word], 1   ; change flag
+    jmp     next_char
+
+ws_word_check:
+    cmp     BYTE [in_word], 1       ; if not new word, continue check
+    je      continue_ws_word_check
+
+start_ws_new_word:
+    mov     dword [word_offset], 0  ; for new word, start reading from the start of ws_word
+    mov     BYTE [good_word], 1     ; for new word, assume good word
+
+continue_ws_word_check:
+    mov     ebx, ws_word            
+    add     ebx, [word_offset]      ; ebx = current char at ws_word
+
+    cmp     BYTE [ebx], 0           ; check if ebx point on ws_word ending
+    je      bad_word                ; if so, its a bad word
+    
+    movzx   eax, BYTE [eax]         ; eax = curr char from buff. eax will reset in next char, needed to perfom cmp
+    cmp     BYTE [ebx], al          ; check for equal chars on ws_word and curr buffer
+    jne     bad_word
+    
+    inc     dword [word_offset]     ; word is good, increment
+    jmp     next_char               ; and check next char
 
 not_in_word:
-    mov     BYTE [in_word], 0
-    jmp     incrementor
-
-check_and_inc_in_word:
-    cmp     BYTE [in_word], 1
-    je      incrementor
+    mov     BYTE [in_word], 0       ; reset in-word for next word apperance
     
-    inc     dword [word_count]
-    mov     BYTE [in_word], 1
+    cmp     BYTE [ws_flag], 0       
+    je      next_char
 
-incrementor:
+continue_ws_not_in_word:
+    mov     ebx, ws_word
+    add     ebx, dword [word_offset]    ; ebx = curr ws_word char
+
+    cmp     BYTE [ebx], 0               ; check if ws_word reached end
+    jne     bad_word                    ; if not, its bad word
+
+    cmp     BYTE [good_word], 1         ; check if good word
+    jne      next_char                  ; if not, skip to next char
+
+    inc     dword [word_count]          ; word is good, increment count
+    jmp     bad_word                    ; prevent double spaces to count word twice
+
+bad_word:
+    mov     BYTE [good_word], 0
+
+next_char:
     inc     edx
     jmp     count_words
-
-
 
 write_count:
     push    dword [word_count]
@@ -113,12 +183,12 @@ open_file:
     push    0           ; open file for read-only
     push    ecx         ; name of file
     call    open
-    mov     ebx, eax    ; fd
+    mov     dword [file_des], eax    ; file_des
 
 read_file:
     push    50          ; read 50 chars at a time
     push    buff
-    push    ebx         ; fd
+    push    dword [file_des]         ; file_des
     call    read
 
 check_EOF:
@@ -134,7 +204,7 @@ write_file:
     jmp     read_file
 
 close_file:
-    push    ebx     ; fd
+    push    dword [file_des]     ; fd
     call    close
 
 exit:
