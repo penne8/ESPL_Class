c147b9b78a205692d72026afa8fe4fbe6d6da231
penne8 <penne8>
2021-12-05 13:27:27 +0200

    task0

--- lab6/task1/myshell.c
+++ lab6/task1/myshell.c
@@ -9,30 +9,58 @@
 #define PATH_MAX 4096
 #define FREE(X) if(X) free((void*)X)
 
-void execute(cmdLine *pCmdLine){
-    int err = execvp(pCmdLine->arguments[0], pCmdLine->arguments);
-    if (err == -1){
-        perror("execv failed");
+int shell_command(cmdLine *pCmdLine){
+    char *command = pCmdLine->arguments[0];
+    if(strcmp(command, "cd")==0){
+        if(pCmdLine->argCount == 1)
+            chdir(getenv("HOME"));
+        else
+            chdir(pCmdLine->arguments[1]);
+            char cwd[PATH_MAX];
+        getcwd(cwd, PATH_MAX);
+        printf("\ncurrent working directory:\n%s\n", cwd);
+        return 1;
     }
+
+    return 0;
+}
+
+void execute(cmdLine *pCmdLine){
+    if(!shell_command(pCmdLine)){
+        pid_t pid = fork();
+        if(pid > 0){
+            execvp(pCmdLine->arguments[0], pCmdLine->arguments);
+        }
+        else{
+            if(pCmdLine->blocking){
+                waitpid(pid, NULL, 0);
+            }
+
+        }
+    }
+    
 }
 
 int main(int argc, char **argv) {
     int should_exit = 0;
     while(!should_exit){
+
         char cwd[PATH_MAX];
         getcwd(cwd, PATH_MAX);
-        printf("current working directory:\n%s\n", cwd);
+        printf("\ncurrent working directory:\n%s\n", cwd);
+
         char sCmdLine[2048];
         fgets(sCmdLine, 2048, stdin);
-        if(strcmp(sCmdLine, "quit") == 0){
+        cmdLine *pCmdLine = parseCmdLines(sCmdLine);
+
+        if(strcmp(pCmdLine->arguments[0], "quit") == 0){
+            printf("exiting...\n");
             should_exit = 1;
-            printf("quiting...\n");
         }
         else{
-            cmdLine *pCmdLine = parseCmdLines(sCmdLine);
             execute(pCmdLine);
-            FREE(pCmdLine);
         }
+        FREE(pCmdLine);
     }
     return 0;
 }
