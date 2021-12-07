06ffd87f3e0932191e6ea48ea920f7a93aea7e27
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-12-07 03:54:18 +0200

    working on fix

--- lab6/task2/task2.c
+++ lab6/task2/task2.c
@@ -6,103 +6,92 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define HISTORY_SIZE 6
+#define HISTORY_SIZE 5
 #define MAX_READ 2048
 #define TRUE 1
 
+#define FREE(X) if(X) free((void*)X)
+
 pid_t pid;
-char* h_array[HISTORY_SIZE];     // history list of commands
+char *h_array[HISTORY_SIZE];     // history list of commands
 int h_count = 0;                // number of total commands
 int h_pointer = 0;              // current command index in history list
 
-void free_history(){
+void free_history() {
     for (int i = 0; i < HISTORY_SIZE; i++)
-        free(h_array[i]);
+        FREE(h_array[i]);
 }
 
-void print_history(){
-    int h_curr_pointer = h_pointer + 2;             // current command pointer inside the h_array
-    int h_command_index = h_count - HISTORY_SIZE;   // current command index in shell
-
-    // check edge case for h_array not full
-    if(h_count < HISTORY_SIZE){
-        h_curr_pointer = 0;
-        h_command_index = 0;
-    }
-
-    /*
-    continue untill curr h_position.
-    (over all, HISTORY_SIZE-2 commands will be printed, all commands
-    in h_array, but without the calling history command itself and
-    the oldest command that will be deleted when the new command will be called.
-    */
-    while(h_command_index != h_count-1){    // -1 to fit count size to index
-        if(h_curr_pointer == HISTORY_SIZE)
-            h_curr_pointer = 0;
-        
-        cmdLine* pCmdLine = parseCmdLines(h_array[h_curr_pointer]);
-
-        printf("%d)", h_command_index);
-        for(int j=0; j<pCmdLine->argCount; j++)
-            printf(" %s", pCmdLine->arguments[j]);
-        printf("\n");
-
-        freeCmdLines(pCmdLine);
-        h_curr_pointer++;
-        h_command_index++;
-    }
+void add_history(char *command) {
+    FREE(h_array[h_pointer]);
+    h_array[h_pointer] = (char *) calloc(MAX_READ, 1);
+    strcpy(h_array[h_pointer], command);
+    h_pointer++;
+    h_pointer %= HISTORY_SIZE;
+    h_count++;
 }
 
-void execute(char* sCmdLine){
+void print_history() {
+    int curr = h_pointer;
+    int count = (h_count < HISTORY_SIZE) ? 0 : (h_count - HISTORY_SIZE);
+    do {
+        if (h_array[curr]) {
+            printf("%d) %s\n", count, h_array[curr]);
+            count++;
+        } else if (h_count >= HISTORY_SIZE)
+            count++;
+        curr++;
+        curr %= HISTORY_SIZE;
+    } while (curr != h_pointer);
+}
 
-    cmdLine* pCmdLine = parseCmdLines(sCmdLine);
+void execute(cmdLine *pCmdLine) {
 
     // check if first char of first argument is '!'
-    if(pCmdLine->arguments[0][0] == 33){
-        int restored_command_index = atoi(pCmdLine->arguments[0]+sizeof(char));
-        if(restored_command_index >= h_count || restored_command_index < h_count - HISTORY_SIZE)
-            fprintf(stderr,"ERROR: history command number '%d' not found. use 'history' command for valid options.\n", restored_command_index);
-        else{
-            int real_array_index = restored_command_index % HISTORY_SIZE;
-            // check edge case for h_array not full
-            if(h_count < HISTORY_SIZE)
-                real_array_index = restored_command_index;
+    if (pCmdLine->arguments[0][0] == 33) {
+        int restored = atoi(pCmdLine->arguments[0] + sizeof(char));
+        if (restored >= h_count || restored < h_count - HISTORY_SIZE) {
+            fprintf(stderr, "ERROR: history command number '%d' ", restored);
+            fprintf(stderr, "not found. use 'history' command for valid options.\n");
+        } else {
+            char *cmd_str = h_array[restored % HISTORY_SIZE];
+            cmdLine *cmd = parseCmdLines(cmd_str);
+            execute(cmd);
+            add_history(cmd_str);
+            freeCmdLines(cmd);
 
-            printf("real_array_index:       %d ---------\n", real_array_index);
-            printf("restored_command_index: %d ---------\n", restored_command_index);
-            printf("h_count:                %d ---------\n", h_count);
-
-            execute(h_array[real_array_index]);
         }
+        return;
     }
 
-    // cd function
-    else if(strcmp(pCmdLine->arguments[0], "cd") == 0){
+        // cd function
+    else if (strcmp(pCmdLine->arguments[0], "cd") == 0) {
         char err = 1;
 
-        if(pCmdLine->argCount == 1){
+        if (pCmdLine->argCount == 1) {
             err = chdir(getenv("HOME"));
-        }
-        else if(pCmdLine->argCount == 2){
+        } else if (pCmdLine->argCount == 2) {
             err = chdir(pCmdLine->arguments[1]);
         }
 
-        if(err)
-            fprintf(stderr, "ERROR: Unknown direcrtory\n");
+        if (err)
+            fprintf(stderr, "ERROR: Unknown directory\n");
+        return;
     }
 
-    // history function
-    else if(strcmp(pCmdLine->arguments[0], "history") == 0){
+        // history function
+    else if (strcmp(pCmdLine->arguments[0], "history") == 0) {
         print_history();
+        return;
     }
 
-    // general function - non shell
-    else{
+        // general function - non shell
+    else {
 
         if ((pid = fork()) == -1)
-            fprintf(stderr,"ERROR: fork error\n");
+            fprintf(stderr, "ERROR: fork error\n");
 
-        else if(pid == 0){ // code executed by child
+        else if (pid == 0) { // code executed by child
 
 
             // execute
@@ -110,51 +99,44 @@ void execute(char* sCmdLine){
 
             // if execvp return, it failed
             printf("ERROR: %s command not found\n", pCmdLine->arguments[0]);
-            
+
             // free all memory and exit
             free_history();
             freeCmdLines(pCmdLine);
             _exit(0);
-        }
-        else if(pCmdLine->blocking){ // code executed by parent
+        } else if (pCmdLine->blocking) { // code executed by parent
             waitpid(pid, NULL, 0);
-        } 
+        }
     }
-
-    freeCmdLines(pCmdLine);
 }
 
 int main(int argc, char **argv) {
 
-    while(TRUE){
-        
-        // print current working directory 
-        char cwd[PATH_MAX];
+    char cwd[PATH_MAX];
+    while (TRUE) {
+
+        // print current working directory
         getcwd(cwd, PATH_MAX);
         printf("MyShell~%s$ ", cwd);
 
         // get user command and save in history array
-        h_array[h_pointer] = (char *) malloc(MAX_READ);
-        fgets(h_array[h_pointer], MAX_READ, stdin);
+        char input[MAX_READ];
+        fgets(input, MAX_READ, stdin);
+        input[strcspn(input, "\n")] = 0; // Removing trailing newline
 
         // quit or execute
-        if(strcmp(h_array[h_pointer], "quit\n") == 0){
+        if (strcmp(input, "quit") == 0) {
             printf("exiting...\n");
             // free history memory
             free_history();
             break;
         }
-        else{
-            h_count++;
-
-            execute(h_array[h_pointer]);
-
-            // increment pointer, if reached to the end, start from begining
-            h_pointer++;
-            if(h_pointer == HISTORY_SIZE)
-                h_pointer = 0;
-        }
-
+        if(strlen(input)==0)
+            continue;
+        cmdLine *cmd = parseCmdLines(input);
+        execute(cmd);
+        add_history(input);
+        freeCmdLines(cmd);
     }
     return 0;
 }
