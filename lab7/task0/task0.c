6c8da186696f087591224e835ce5204b64f734bc
penne8 <penne8>
2021-12-08 22:31:42 +0200

    task0 complete

--- lab7/task0/task0.c
+++ lab7/task0/task0.c
@@ -10,31 +10,34 @@ int main(int argc, char *argv[])
     pid_t cpid;
     char buf[32];
 
-    if (pipe(pipefd) == -1) /* An error has occurred. */
+    if (pipe(pipefd) == -1)         /* An error has occurred. */
     {
         fprintf(stderr, "%s", "The call to pipe() has failed.\n");
         exit(EXIT_FAILURE);
     }
-    if ((cpid = fork()) == -1) /* An error has occurred. */
+    if ((cpid = fork()) == -1)      /* An error has occurred. */
     {
         fprintf(stderr, "%s", "The call to fork() has failed.\n");
         exit(EXIT_FAILURE);
     }
     if (cpid == 0)
     { /* Child */
-        close(pipefd[0]); /* Close unused read end */
+        close(pipefd[0]);           /* Close unused read end */
+        
         write(pipefd[1], "hello", strlen("hello"));
-        close(pipefd[1]); /* Closing creates the EOF marker. */
+        close(pipefd[1]);           /* Closing creates the EOF marker. */
 
         _exit(EXIT_SUCCESS);
     }
     else
     { /* Parent */
-        close(pipefd[1]); /* Close unused write end */
-        wait(NULL); /* Parent waits for the child to terminate */
+        close(pipefd[1]);           /* Close unused write end */
+
+        waitpid(cpid, NULL, 0);     /* Parent waits for the child to terminate */
         read(pipefd[0], buf, 32);
         printf("%s\n", buf);
         close(pipefd[0]);
+
         exit(EXIT_SUCCESS);
     }
     return 0;
